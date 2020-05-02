c7fbd7305bffaa4daf4067f93a1f65b3c64037ba
ajafik <oluwafikayoajayi@gmail.com>
2020-05-02 14:21:37 +0100

    added API config

---
+++ config/config.js
@@ -0,0 +1,22 @@
+var config = require("./config.json");
+class Config {
+
+    //API SETTINGS
+    static get get_app_name() {
+        return config.app_name;
+    }
+
+    static get get_app_port() {
+        return config.port;
+    }
+
+    static get get_app_version() {
+        return config.app_version;
+    }
+
+    static get get_api_version() {
+        return config.api_version || "/v1/api";
+    }
+}
+
+module.exports = Config;
